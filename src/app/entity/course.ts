48cd6198978e43617de802662e55b37d32cbec6d
zzxn <zzxnhackman@gmail.com>
2019-06-17 18:14:36 +0800

    fix: miss some classes at last commit

---
+++ src/app/entity/course.ts
@@ -0,0 +1,7 @@
+export class Course {
+  id: number;
+  name: string;
+  teacherId: string;
+  description: string;
+  image: string;
+}
---
+++ src/app/service/course.service.ts
@@ -0,0 +1,32 @@
+import {Injectable} from '@angular/core';
+import {Observable, Observer} from 'rxjs';
+import {Course} from '../entity/course';
+import {delay, timeout} from 'rxjs/operators';
+import {HttpClient} from '@angular/common/http';
+import {AuthService} from './auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CourseService {
+
+  constructor(private http: HttpClient, private authService: AuthService) {
+  }
+
+  getAllCourseOfMine(): Observable<Course[]> {
+    const uuid = localStorage.getItem('uuid');
+    if (uuid === null) {
+      return Observable.create((observer: Observer<object>) => {
+        observer.error('未登录');
+      });
+    }
+    return this.http.get<Course[]>('http://localhost:8080/teacher/' + uuid + '/course', {
+      headers: {
+        'Authorization': 'Bearer ' + this.authService.getToken()
+      }
+    }).pipe(
+      delay(300),
+      timeout(5000)
+    );
+  }
+}
